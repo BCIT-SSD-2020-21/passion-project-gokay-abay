0ab7817993ddd0014278a522d81df98e8a08b82c
Gökay Abay <abaygokay@gmail.com>
2021-04-11 15:11:21 -0700

    Navbar comp added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css"
 import LoginPage from "./pages/LoginPage/LoginPage"
+import Navbar from "./components/Navbar/Navbar"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 function App() {
   return (
     <Router className="App">
+      <Navbar />
       <Switch>
         <Route exact path="/">
           <LoginPage />
