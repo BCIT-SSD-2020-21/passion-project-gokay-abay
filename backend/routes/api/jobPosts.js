54cc9c358c5ac45285a745ce9f1972742ed488af
Gökay Abay <abaygokay@gmail.com>
2021-04-10 17:28:16 -0700

    Endpoint for creating job post entry

--- backend/routes/api/jobPosts.js
+++ backend/routes/api/jobPosts.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const auth = require("../../middleware/auth")
+const { check, validationResult } = require("express-validator/")
 const JobPost = require("../../models/JobPost")
 
 // @route   GET api/posts
@@ -20,5 +21,47 @@ router.get("/", auth, async (req, res) => {
     res.status(500).send("Server error")
   }
 })
+
+// @route   POST api/posts
+// @desc    Create a job post entry
+// @access  Private
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("title", "Job title is required").not().isEmpty(),
+      check("company", "Company name is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    try {
+      let newPost = await JobPost.findOne({ user: req.user.id })
+
+      if (newPost) {
+        newPost = await JobPost.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: req.body },
+          { new: true }
+        )
+        return res.json(newPost)
+      }
+
+      const postObj = req.body
+
+      newPost = new JobPost({ ...postObj, user: req.user.id })
+      await newPost.save()
+      res.json(newPost)
+    } catch (err) {
+      console.error(err.message)
+      res.status(500).send("Server error")
+    }
+  }
+)
 
 module.exports = router
