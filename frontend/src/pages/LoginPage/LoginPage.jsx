ccaa678bfe5604acf631a46ff56334317fbf4c3f
Gökay Abay <abaygokay@gmail.com>
2021-04-12 11:45:05 -0700

    Login/register users on website

--- frontend/src/pages/LoginPage/LoginPage.jsx
+++ frontend/src/pages/LoginPage/LoginPage.jsx
@@ -1,10 +1,28 @@
 import React from "react"
 import Login from "../../components/Login/Login"
+import { authUser, registerUser } from "../../api/auth"
 
 const LoginPage = () => {
+  const onSubmit = async ({ type, email, name, password }) => {
+    if (type === "login") {
+      const result = await authUser({
+        email,
+        password,
+      })
+      console.log(result)
+    } else {
+      const result = await registerUser({
+        name,
+        email,
+        password,
+      })
+      console.log(result)
+    }
+  }
+
   return (
     <div>
-      <Login />
+      <Login onSubmit={onSubmit} />
     </div>
   )
 }
