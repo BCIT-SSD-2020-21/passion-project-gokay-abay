5254933b8e5099ddea23a820868c47be4e15bd3c
Gökay Abay <abaygokay@gmail.com>
2021-04-13 09:44:43 -0700

    Add progress indicator

--- frontend/src/pages/DashboardPage/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage/DashboardPage.jsx
@@ -1,9 +1,22 @@
 import React, { useEffect, useState } from "react"
+import { makeStyles } from "@material-ui/core/styles"
 import JobPostTable from "../../components/JobPostTable/JobPostTable"
 import { getAllJobPosts } from "../../api/jobPosts"
 import CircularProgress from "@material-ui/core/CircularProgress"
 
+const useStyles = makeStyles((theme) => ({
+  loading: {
+    height: "50vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+}))
+
 const DashboardPage = () => {
+  const classes = useStyles()
+
   const [jobposts, setJobposts] = useState([])
   const [loading, setLoading] = useState(true)
 
@@ -11,13 +24,22 @@ const DashboardPage = () => {
     ;(async () => {
       const res = await getAllJobPosts()
       setJobposts(res)
-      setLoading(false)
+      setTimeout(() => {
+        setLoading(false)
+      }, 1000)
     })()
   }, [])
 
   return (
     <div>
-      {loading ? <CircularProgress /> : <JobPostTable jobposts={jobposts} />}
+      {loading ? (
+        <div className={classes.loading}>
+          <h2>Loading...</h2>
+          <CircularProgress />
+        </div>
+      ) : (
+        <JobPostTable jobposts={jobposts} />
+      )}
     </div>
   )
 }
