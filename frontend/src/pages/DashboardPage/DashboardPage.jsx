94a8252e9734960110502692528c0a32192560d7
Gökay Abay <abaygokay@gmail.com>
2021-04-13 14:50:16 -0700

    Edit, Add buttons

--- frontend/src/pages/DashboardPage/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage/DashboardPage.jsx
@@ -5,14 +5,28 @@ import CircularProgress from "@material-ui/core/CircularProgress"
 import JobPostTable from "../../components/JobPostTable/JobPostTable"
 import Modal from "../../components/Modal/Modal"
 import NewJobPost from "../../components/NewJobPost/NewJobPost"
+import { IconButton } from "@material-ui/core"
+import Fab from "@material-ui/core/Fab"
+import EditIcon from "@material-ui/icons/Edit"
+import AddIcon from "@material-ui/icons/Add"
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: "1em",
+    // backgroundColor: "#F5F5F5",
+    minHeight: "100vh",
+  },
   loading: {
     height: "50vh",
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
+  },
+  buttons: {
+    display: "flex",
+    justifyContent: "flex-end",
+    margin: "1em",
   },
 }))
 
@@ -36,6 +50,10 @@ const DashboardPage = () => {
     setTimeout(() => setLoading(false), 1000)
   }
 
+  const getRowData = (data) => {
+    console.log(data)
+  }
+
   return (
     <div>
       {loading ? (
@@ -44,12 +62,20 @@ const DashboardPage = () => {
           <CircularProgress />
         </div>
       ) : (
-        <>
-          <JobPostTable jobposts={jobposts} />
+        <div className={classes.root}>
+          <div className={classes.buttons}>
+            <Fab color="secondary" aria-label="edit">
+              <EditIcon />
+            </Fab>
+            <Fab color="primary" aria-label="edit">
+              <AddIcon />
+            </Fab>
+          </div>
+          <JobPostTable jobposts={jobposts} getRowData={getRowData} />
           <Modal>
             <NewJobPost onSubmit={submit} />
           </Modal>
-        </>
+        </div>
       )}
     </div>
   )
