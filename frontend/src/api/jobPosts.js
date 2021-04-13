81af900b259703f96d9be184ebda32797d480b67
Gökay Abay <abaygokay@gmail.com>
2021-04-13 12:43:40 -0700

    api call: create new job post func

--- frontend/src/api/jobPosts.js
+++ frontend/src/api/jobPosts.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 import setAuthToken from "../utils/setAuthToken"
+
 const url = "http://localhost:5000"
 
 export async function getAllJobPosts() {
@@ -9,6 +10,19 @@ export async function getAllJobPosts() {
 
   try {
     const res = await axios.get(url + "/api/jobposts")
+    return res.data
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export async function createJobPost(body) {
+  if (localStorage.token) {
+    setAuthToken(localStorage.token)
+  }
+
+  try {
+    const res = await axios.post(url + "/api/jobposts", body)
     return res.data
   } catch (err) {
     console.log(err)
